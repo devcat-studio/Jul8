4075606cc8157f054cb8a7715757c44b523aa18e
Seungjae Lee <distynova@gmail.com>
2018-01-25 22:17:59 +0900

    사소하게 다듬음.

--- Sample/main.ts
+++ Sample/main.ts
@@ -1,6 +1,8 @@
 ﻿/// <reference path='sample_templates.g.ts' />
 
-// 상속해서 만든 예제. 굳이 상속하지 않아도 상관없다.
+//-------------------------------------------------------
+// 상속해서 만든 예제.
+// 꼭 상속해서 만들어야만 하는 것은 아니다.
 class MyTable extends MyTable_d {
     count: number = 0;
 
@@ -9,13 +11,19 @@ class MyTable extends MyTable_d {
     }
 
     addNewItem(): void {
+        // 이 예제에서는 굳이 상속을 사용하지 않음.
+        // 리스트 아이템에서 상속을 사용하고 싶다면 아래와 같이 하면 된다.
+        //var tr = <MyTable_TR>this.listOf_TR.add(MyTable_TR);
+
         var tr = this.listOf_TR.add(MyTable_TR_d);
-        tr.num.text(++this.count);
-        tr.content.text("칸은 만들었는데 딱히 쓸 말이 없는 " + this.count + "번째 행");
+        let n = ++this.count;
+        tr.num.text(n);
+        tr.content.text("칸은 만들었는데 딱히 쓸 말이 없는 " + n + "번째 행");
         tr.btnRemove.click(() => this.listOf_TR.remove(tr));
     }
 }
 
+//-------------------------------------------------------
 let g_jul8: Jul8.TemplateHolder;
 
 $(document).ready(
