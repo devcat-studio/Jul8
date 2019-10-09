31aaa169457ce03d8205ab2e6daa44561cbdee41
Seungjae Lee <distynova@gmail.com>
2019-10-09 23:35:07 +0900

    TypeScript 개발환경을 VS에서 npm으로 옮김. (빌드 깨져있음)
    + TypeScript 버전 3.6.3으로 올림.
    + jquery.d.ts를 npm 것으로 교체.

---
+++ gruntfile.js
@@ -0,0 +1,33 @@
+module.exports = function (grunt) {
+    // 우리가 지은 이름과 태스크 이름을 구분하기 어려워서
+    // 우리가 지은 이름에는 언더바(_)를 붙임
+
+    grunt.initConfig({
+        // https://github.com/sindresorhus/grunt-concurrent
+        concurrent: {
+            target1_: ['watch:Sample_Templates_']
+        },
+
+        // https://github.com/gruntjs/grunt-contrib-watch
+        watch: {
+            Sample_Templates_: {
+                files: ["Sample/html/sample_templates.html"],
+                tasks: ["exec:Sample_Templates_"],
+            }
+        },      
+
+        // https://www.npmjs.com/package/grunt-exec
+        exec: {
+            Sample_Templates_: {
+                cwd: '.',
+                command: '"Compiler/bin/Jul8Compiler.exe" "Sample/jul8config.json"'
+            }
+        },
+    });
+
+    grunt.loadNpmTasks('grunt-exec');
+    grunt.loadNpmTasks('grunt-concurrent');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('default', ['watch']);
+}
